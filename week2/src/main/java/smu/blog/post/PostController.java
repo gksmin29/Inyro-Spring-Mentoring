dcb3928c25624536bc65804110429a791095f739
Hanmin <rlemdhs2747@naver.com>
2026-09-23 00:24:10 +0900

    Cors 설정 추가

--- week2/src/main/java/smu/blog/post/PostController.java
+++ week2/src/main/java/smu/blog/post/PostController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class PostController {
 
     private List<Post> postList = new ArrayList<>();
